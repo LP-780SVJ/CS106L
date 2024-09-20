3ccd31f3e7183f2cab4a3881e7bad5201f11ee31
LeeSQ <2501886099@qq.com>
2024-09-20 05:23:39 +0000

    C++_setw()

--- assignments/Test/test.cpp
+++ assignments/Test/test.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-int main() {    
-    ofstream myfile;
-    myfile.open("myFile.txt");
-    myfile << "Hello, world!" << endl;
-    myfile.close();
+const int NUM_LINES = 4;
+const int NUM_COLUMNS = 3;
+const int COLUMN_WIDTH = 20;
+
+void PrintTableBody() {
+    ifstream input("table-data.txt");
+
+    for (int k=0; k<NUM_LINES; ++k){
+        int intValue;
+        double doubleValue;
+        input >> intValue >> doubleValue;
+
+        cout << setw(COLUMN_WIDTH) << (k+1) << " | "
+             << setw(COLUMN_WIDTH) << intValue << " | "
+             << setw(COLUMN_WIDTH) << doubleValue << endl;
+    }
+}
+
+
+int main() {   
+    //PrintTableHeader();
+    PrintTableBody();
+    return 0;
+
+    // cout << setw(10) << "hello" << endl;
     return 0;
 }
