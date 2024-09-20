71dc1589e3913b2113252b08faf751187d812c98
LeeSQ <2501886099@qq.com>
2024-09-20 06:22:33 +0000

    C++_setw2

--- assignments/Test/test.cpp
+++ assignments/Test/test.cpp
@@ -8,9 +8,28 @@ const int NUM_LINES = 4;
 const int NUM_COLUMNS = 3;
 const int COLUMN_WIDTH = 20;
 
+void PrintTableHeader() {
+    /*Print the ---...---+- pattern for all but the last column*/
+    for (int column = 0; column < NUM_COLUMNS-1; ++column){
+        for (int i = 0; i < COLUMN_WIDTH; ++i){
+            cout << '-';
+        }
+        cout << "-+-``";
+    }
+
+    /*Now print the ---...--- pattern for the last column*/
+    for(int k = 0; k < COLUMN_WIDTH; ++k){
+        cout << '-';
+    }
+    cout << endl;
+}
+
 void PrintTableBody() {
     ifstream input("table-data.txt");
+    /*No error-checking here, but we should be sure to do this in any real program*/
 
+
+    /*Loop over the lines in the file reading data*/
     for (int k=0; k<NUM_LINES; ++k){
         int intValue;
         double doubleValue;
@@ -24,7 +43,7 @@ void PrintTableBody() {
 
 
 int main() {   
-    //PrintTableHeader();
+    PrintTableHeader();
     PrintTableBody();
     return 0;
 
