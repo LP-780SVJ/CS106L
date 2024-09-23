b61f339cb45bb2b6fa51ce2ed10f1199949e8abe
LeeSQ <2501886099@qq.com>
2024-09-23 12:50:30 +0000

    C++_HasHexLetters_Ans

--- assignments/Test/HasHexLetters.cpp
+++ assignments/Test/HasHexLetters.cpp
@@ -12,13 +12,30 @@ bool HasHexLetters(int num) {
     stringstream ss;
     ss << hex << num;                               //hex 是一个格式化操控符，它指示流在输出数字时使用十六进制格式。
     string hexStr = ss.str();
-    cout << "Hexadecimal representation of " << num << " is " << hexStr << endl;
+    //cout << "Hexadecimal representation of " << num << " is " << hexStr << endl;
     for (char c : hexStr) {
         if (isalpha(c)) {
             return true;
         }
     }
     return false;
+}
+
+/*Answers*/
+bool HasHexLetters_Ans(int value) {
+    stringstream converter;
+    converter << hex << value;
+    int dummy;
+    converter >> dec >> dummy;
+
+    if(converter.fail()) {
+        return true;
+    }
+
+    char leftover;
+    converter >> leftover;
+
+    return !converter.fail();
 }
 
 int main() {
@@ -36,6 +53,13 @@ int main() {
     cout << "num4: " << num4 << " has hex letters: " << HasHexLetters(num4) << endl;
     cout << "num5: " << num5 << " has hex letters: " << HasHexLetters(num5) << endl;
     cout << "num6: " << num6 << " has hex letters: " << HasHexLetters(num6) << endl;
+
+    cout << "num1: " << num1 << " has hex letters: " << (HasHexLetters_Ans(num1) ? "Yes" : "No") << endl;
+    cout << "num2: " << num2 << " has hex letters: " << HasHexLetters_Ans(num2) << endl;
+    cout << "num3: " << num3 << " has hex letters: " << HasHexLetters_Ans(num3) << endl;
+    cout << "num4: " << num4 << " has hex letters: " << HasHexLetters_Ans(num4) << endl;
+    cout << "num5: " << num5 << " has hex letters: " << HasHexLetters_Ans(num5) << endl;
+    cout << "num6: " << num6 << " has hex letters: " << HasHexLetters_Ans(num6) << endl;
     
 
     return 0;
