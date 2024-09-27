5e4303c86fe8e990ff7a41a0b1f04b2326fbe18d
LeeSQ <2501886099@qq.com>
2024-09-27 05:31:38 +0000

    cmake_debug_test

---
+++ assignments/Test/cmake_test/test.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+/*
+2
+3 1 2 2
+1 2 3 7
+*/
+
+int main() {
+  cout << "hello1" << endl;
+
+  for (int i = 0; i < 10; i++) {
+    cout << i << endl;
+  }
+
+  return 0;
+}
